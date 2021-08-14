1f865bc52c68d7f4cc7c95131031c01764a77a6d
sueskind <52210599+sueskind@users.noreply.github.com>
2021-08-14 14:28:11 +0200

    Restructure, Measure endpoint

--- controller/main.ino
+++ controller/main.ino
@@ -1,6 +1,42 @@
 #include <WiFi.h>
+#include <WebServer.h>
+#include <ArduinoJson.h>
 
 #define UPDATE_INTERVAL 100
+#define SENSORS_COUNT 4
+
+byte sensorPins[] = {34, 35, 32, 33};
+byte pumpPins[] = {19, 18, 5, 17};
+
+WebServer server(80); // port
+long lastUpdate = 0;
+
+
+// endpoints
+void api_measure() {
+    StaticJsonDocument<64> json;
+
+    for (byte i = 0; i < SENSORS_COUNT; i++) {
+        json.add(analogRead(sensorPins[i]));
+    }
+
+    String body;
+    serializeJson(json, body);
+
+    server.send(200, "application/json", body);
+    Serial.println("HTTP 200 GET /measure");
+}
+
+void api_water() {
+    server.send(200);
+    Serial.println("HTTP 200 GET /water");
+}
+
+void api_notFound() {
+    server.send(404, "text/plain", "Not found");
+    Serial.println("HTTP 404 GET ?");
+}
+
 
 void setup() {
     Serial.begin(115200);
@@ -29,10 +65,11 @@ void setup() {
     Serial.println("HTTP server started\n");
 }
 
-long lastUpdate = 0;
+
 void loop() {
     if (millis() - lastUpdate > UPDATE_INTERVAL) {
         server.handleClient();
         lastUpdate = millis();
     }
+
 }
